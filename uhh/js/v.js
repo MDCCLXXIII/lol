0fb7a2958cc6ff065f21e5a1ac6d623b7aabd4f5
Ciya <165425046+MDCCLXXIII@users.noreply.github.com>
2025-05-22 01:14:34 +0700

    Update v.js

--- uhh/js/v.js
+++ uhh/js/v.js
@@ -1,89 +1,11 @@
-    body {
-      background-color: #0C0C0C;
-      color: white;
-      text-align: center;
-      font-family: 'Poiret One', sans-serif;
-      user-select: none;
-      margin: 0;
-      padding: 0;
-    }
-
-    img {
-      width: 60%;
-      max-width: 240px;
-      height: auto;
-      margin-top: 40px;
-    }
-
-    h1 {
-      color: red;
-      text-shadow: 1px 1px 1px #000;
-      font-size: 2rem;
-      margin-top: 20px;
-    }
-
-    p {
-      margin: 10px auto;
-      font-size: 1rem;
-      text-shadow: 1px 1px 1px #000;
-      max-width: 911px;
-      line-height: 1.2;
-      padding: 0 10px;
-    }
-
-    button {
-      margin: 10px auto 40px;
-      padding: 5px 20px;
-      font-size: 1rem;
-      text-shadow: 1px 1px 1px #000;
-      cursor: pointer;
-      border: none;
-      border-radius: 5px;
-      background-color: #ff0000;
-      color: white;
-    }
-
-      button:hover {background-color: #aa0000}
-      button:active {
-      background-color: #700000;
-      box-shadow: 0 5px #444;
-      transform: translateY(4px);
-    }
-
-    @media (min-width: 600px) {
-      h1 {
-        font-size: 1.5rem;
-      }
-
-      p {
-        font-size: 1.1rem;
-      }
-
-      button {
-        font-size: 1.0rem;
-        padding: 5px 20px;
-      }
-
-      img {
-        max-width: 230px;
+    const music = document.getElementById("lxrdk1773n");
+    function uwu() {
+      if (music.paused) {
+        music.play();
+      } else {
+        music.pause();
       }
     }
-
-    @media (min-width: 1024px) {
-      h1 {
-        font-size: 2rem;
-      }
-
-      p {
-        font-size: 1.2rem;
-      }
-
-      button {
-        font-size: 1.0rem;
-        padding: 5px 20px;
-      }
-
-      img {
-        max-width: 240px;
-      }
-    }
+    window.onbeforeunload = function () {
+      return "lol";
+    };
